cdcf078468de1b9c09b38727ba4292823d730352
Aral Roca Gomez <aral-rg@hotmail.com>
2021-06-20 19:21:28 +0200

    comment fn

--- helpers/downloadSubscriptions.ts
+++ helpers/downloadSubscriptions.ts
@@ -1,19 +1,19 @@
 export default async function downloadSubscriptions(subscriptions, title) {
-  const filename = `${title}.csv`
-  const Papa = await import('papaparse').then((m) => m.default)
-  const csv = Papa.unparse({
-    header: true,
-    data: subscriptions,
-  })
-  const blob = new Blob([csv])
-  if (window.navigator.msSaveOrOpenBlob) {
-    return window.navigator.msSaveBlob(blob, filename)
-  }
+  // const filename = `${title}.csv`
+  // const Papa = await import('papaparse').then((m) => m.default)
+  // const csv = Papa.unparse({
+  //   header: true,
+  //   data: subscriptions,
+  // })
+  // const blob = new Blob([csv])
+  // if (window.navigator.msSaveOrOpenBlob) {
+  //   return window.navigator.msSaveBlob(blob, filename)
+  // }
 
-  const a = window.document.createElement('a')
-  a.href = window.URL.createObjectURL(blob, { type: 'text/plain' })
-  a.download = filename
-  document.body.appendChild(a)
-  a.click()
-  document.body.removeChild(a)
+  // const a = window.document.createElement('a')
+  // a.href = window.URL.createObjectURL(blob, { type: 'text/plain' })
+  // a.download = filename
+  // document.body.appendChild(a)
+  // a.click()
+  // document.body.removeChild(a)
 }
