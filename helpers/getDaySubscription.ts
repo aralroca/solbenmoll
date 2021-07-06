333873d4b4e10492c4045011de9ce8996659cf75
Aral Roca Gomez <aral-rg@hotmail.com>
2021-07-06 21:01:36 +0200

    Implement displacement of subscription

--- helpers/getDaySubscription.ts
+++ helpers/getDaySubscription.ts
@@ -1,11 +1,12 @@
 export default function getDaySubscription(subscription, weekIndex) {
   const daySub = { ...subscription }
   const [year, index] = weekIndex
+  const finalIndex = index + (subscription.displacement || 0)
   let active = false
 
   Object.keys(daySub).forEach((k) => {
     const isActive =
-      daySub[k].count > 0 && index % parseInt(daySub[k].time) === 0
+      daySub[k].count > 0 && finalIndex % parseInt(daySub[k].time) === 0
     active = active || isActive
     daySub[k] = {
       count: isActive ? daySub[k].count : 0,
