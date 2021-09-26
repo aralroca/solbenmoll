20fb3032abcd9b0c517b3779c411ec27dd62cbef
Aral Roca Gomez <aral-rg@hotmail.com>
2021-09-26 11:41:00 +0200

    Fix format XLSX

--- helpers/downloadXLSL.ts
+++ helpers/downloadXLSL.ts
@@ -1,94 +1,110 @@
-/***
-import pandas as pd
-import xlsxwriter
-
-wep = pd.read_csv('wep.csv')
-wep['Excepcions']=wep['Excepcions'].fillna('')
-
-PRC = {'La Tinta': '#f1aee7', 'Mataró': '#6aa84f', 'Brusi': 'lime', 'L’anònima del Clot': 'magenta',
-			 'Besòs Verd': 'red', 'Sòl Ben Moll': 'green' , 'ALBA Sincrotró': '#ffff00', 'El Guinardó':'yellow'}
-llistes = xlsxwriter.Workbook('llistes.xlsx')
-full = llistes.add_worksheet('llistes')
-color_grup = llistes.add_format({'bold': True, })
-cell_format = llistes.add_format({'bold': True, 'bg_color': '#7da7d8', 'font_name': 'Arial', 'font_size':10})
-cell_format1 = llistes.add_format({'bold': True, 'bg_color': '#7da7d8', 'align': 'center', 'font_name': 'Arial', 'font_size':10})
-cell_format2 = llistes.add_format({'align': 'center', 'font_name': 'Arial', 'font_size':10})
-
-def capçalera(PR, i):
-		full.write('B'+str(i), PR, color_grup)
-		full.write('B'+str(i+1), 'nom', cell_format)
-		full.write('C'+str(i+1), 'id', cell_format)
-		full.write('D'+str(i+1), 'excepcions', cell_format)
-		full.write('E'+str(i+1), 'Cistella', cell_format1)
-		full.write('F'+str(i+1), 'Fruita', cell_format1)
-		full.write('G'+str(i+1), 'Ous', cell_format1)
-		full.write('H'+str(i+1), 'CP', cell_format1)
-	  
-def filera(fila, i):
-		cistella = ''
-		if wep['Petita'][i] == 1:
-				cistella = cistella + 'P'
-		if wep['Petita'][i] > 1:
-				cistella = cistella + str(wep['Petita'][i]) + 'P'
-		if wep['Mitjana'][i] == 1:
-				cistella = cistella + 'M'
-		if wep['Mitjana'][i] > 1:
-				cistella = cistella + str(wep['Mitjana'][i]) + 'M'
-		if wep['Gran'][i] == 1:
-				cistella = cistella + 'G'
-		if wep['Gran'][i] > 1:
-				cistella = cistella + str(wep['Gran'][i]) + 'G'
-			  
-		full.write('A'+str(fila), i+1, cell_format2)
-		full.write('B'+str(fila), wep['Nom'][i])
-		full.write('C'+str(fila), wep['Id Usuari'][i])
-		full.write('D'+str(fila), wep['Excepcions'][i])
-		full.write('E'+str(fila), cistella, cell_format2)
-		full.write('F'+str(fila), wep['Fruita'][i], cell_format2)
-		full.write('G'+str(fila), wep['Ous'][i], cell_format2)
-		full.write('H'+str(fila), wep['Patata i Ceba'][i], cell_format2)
-
-
-L = len(wep)
-PR = wep['Punt Recollida'][0]
-color = PRC[wep['Punt Recollida'][0]]
-color_grup = llistes.add_format({'bold': True, 'bg_color': color})
-capçalera(PR,1)
-fila = 3
-
-for i in range(L):
-	  
-		if wep['Punt Recollida'][i] != PR:
-				color = PRC[wep['Punt Recollida'][i]]
-				color_grup = llistes.add_format({'bold': True, 'bg_color': color})
-				PR = wep['Punt Recollida'][i]
-				capçalera(PR,fila)
-				fila = fila + 2
-		try:
-				filera(fila,i)
-				fila = fila + 1
-		except:
-				pass
-	  
-llistes.formats[0].set_font_name('Arial')
-llistes.formats[0].set_font_size(10)
-full.set_column('A:A', 3)
-full.set_column('B:B', 22)
-full.set_column('D:D', 30)
-full.set_column('E:H', 7)
-
-llistes.close()
- */
-
 import { getData } from './downloadCSV'
 import pickupPoints from '../constants/pickpoints'
 
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+const centerLetters = ['E', 'F', 'G', 'H']
+
 // All the Python code before transformed into typescript:
 export default async function downloadXLSL(data, week) {
+	let index = 1
+	const positions = []
+	const sheet = []
 	const json = await getData(data)
-	const XLSX = await import('xlsx')
+	const XLSX = await import('xlsx-js-style')
 	const workbook = XLSX.utils.book_new()
-	const worksheet = XLSX.utils.json_to_sheet(json)
+
+	const jsonObj = json.reduce((acc, row) => {
+		if (!acc[row.PR]) acc[row.PR] = [row]
+		else acc[row.PR].push(row)
+		return acc
+	}, {})
+
+	Object.keys(jsonObj).forEach((PR) => {
+		const position = sheet.push(['', PR, '', '', '', '', '', ''])
+		sheet.push([
+			'',
+			'nom',
+			'id',
+			'excepcions',
+			'Cistella',
+			'Fruita',
+			'Ous',
+			'CP',
+		])
+		const color = pickupPoints
+			.find((p) => p.name === PR)
+			.color.replace('#', '88')
+		positions.push({ position, color })
+		jsonObj[PR].forEach((row) => {
+			sheet.push([
+				index,
+				row.nom,
+				row.id,
+				row.excepcions,
+				row.Cistella,
+				row.Fruita,
+				row.Ous,
+				row.CP,
+			])
+			index += 1
+		})
+	})
+
+	const worksheet = XLSX.utils.aoa_to_sheet(sheet)
+	const font = {
+		sz: 10,
+		name: 'Arial',
+	}
+
+	worksheet['!cols'] = [
+		{ wch: 3 },
+		{ wch: 22 },
+		{},
+		{ wch: 30 },
+		{ wch: 7 },
+		{ wch: 7 },
+		{ wch: 7 },
+		{ wch: 7 },
+	]
+
+	const totalRows = json.length + Object.keys(jsonObj).length * 2
+
+	// All cells are set to the same font
+	letters.forEach((letter) => {
+		for (let i = 1; i <= totalRows; i += 1) {
+			const key = `${letter}${i}`
+			if (!worksheet[key]) continue
+			worksheet[key].s = {
+				font,
+				alignment: centerLetters.includes(letter)
+					? { horizontal: 'center' }
+					: undefined,
+			}
+		}
+	})
+
+	// Set the bgcolor + bold text of the header
+	positions.forEach(({ position, color }) => {
+		worksheet[`B${position}`].s = {
+			font: { fontSize: 10, bold: true },
+			fill: { fgColor: { patternType: 'solid', rgb: color } },
+		}
+		letters.forEach((letter) => {
+			if (letter === 'A') {
+				worksheet[`A${position + 1}`].s = {
+					alignment: { horizontal: 'bottom' }
+				}
+				return
+			}
+			worksheet[`${letter}${position + 1}`].s = {
+				font: { ...font, bold: true },
+				fill: { fgColor: { patternType: 'solid', rgb: 'FF7DA7D8' } },
+				alignment: centerLetters.includes(letter)
+					? { horizontal: 'center' }
+					: undefined,
+			}
+		})
+	})
 
 	XLSX.utils.book_append_sheet(workbook, worksheet, 'llista')
 	XLSX.writeFile(workbook, `${week.name}.xlsx`)
