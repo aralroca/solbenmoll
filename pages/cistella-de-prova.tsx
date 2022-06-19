5a241c92de286dd121a8938f651587a49b52c380
vperezb <victor.perez.berruezo@gmail.com>
2022-06-19 12:27:55 +0200

    Creada pàgina de cistella de prova

---
+++ pages/cistella-de-prova.tsx
@@ -0,0 +1,95 @@
+import useTranslation from 'next-translate/useTranslation'
+import Breadcrumb from '../components/Breadcrumb'
+import PickUpPointsMap from '../components/PickUpPointsMap'
+import InstaIcon from '../components/Icons/Insta'
+import MailIcon from '../components/Icons/Mail'
+import Link from 'next/link'
+
+function CistellaDeProva() {
+  const { t } = useTranslation('common')
+  const title = 'Cistella de prova'
+  const containerStyles = { display: 'flex', alignItems: 'center', margin: 5 }
+  
+  return (
+    <div className="content">
+      <Breadcrumb
+        currentPageName={title}
+        links={[
+          {
+            href: '/',
+            name: 'home',
+          },
+        ]}
+      />
+      <h1>{title}</h1>
+
+      <p>La nostra filosofia i model de negoci es basa en la subscripció de cistelles, ja que permet establir una rel·lació productor-client final que ens dona una estabilitat de demanda que ens permet planificar els cultius i disminuïr el malbaratament. Tot i així, oferim la possibilitat de demanar una cistella de prova per a que pogueu tastar la qualitat i manera de treballar.</p>
+
+      <p>Les verdures de les nostres cistelles són de la nostra horta a Cabrera de Mar i de la coperativa de EcoMaresme que ens fa de proveïdors si ens manca algun producte.</p>
+     
+      <h2>Què inclou la cistella de prova?</h2>
+      <p>Servim cistelles de verdura de temporada, amb la possibilitat d'escollir "extra de fruita" de proveïdors també ecològics. Les mides i opcions són les següents:</p>
+
+      <ul>
+          
+            <li> Cistella mitjana (5kg, 6 productes, 14.5€) </li>
+            <li> Mitja dotzena d'ous (2.3€) </li>
+            <li> Fruita de temporada (2kg, 3 productes, 5.5€)</li>
+          
+      </ul>
+
+      <p> Preu total: <b>23.3€</b></p>
+
+      <Link href="https://buy.stripe.com/test_5kAaIm8DnfHjfKM28d">
+          <a style={{ textDecoration: 'none' }}>
+            <div
+              style={{
+                color: 'white',
+                margin: '30px auto',
+                backgroundColor: '#99b67e',
+                borderRadius: 30,
+                textAlign: 'center',
+                padding: 10,
+                width: '100%',
+                maxWidth: 250,
+              }}
+            >
+              Demanar cistella de prova
+            </div>
+          </a>
+        </Link>
+
+    <h2>On i quan puc recollir la cistella?</h2>
+
+    <p>La cistella la podràs recollir de manera gratuïta en els següents punts de recollida <b>el dimecres de la setmana següent a la comanda</b>. Entre dimecres tarda i dijous. A continuació oferim informació detallada per a cada punt de recollida.</p>
+
+    <PickUpPointsMap />
+          
+    <h2>{t`contact-content.description`}</h2>
+      <div style={containerStyles}>
+        <MailIcon width={18} height={18} />
+        <a
+          style={{ marginLeft: 10 }}
+          href="mailto:solbenmoll@gmail.com"
+          target="_blank"
+        >
+          solbenmoll@gmail.com
+        </a>
+      </div>
+      <div style={containerStyles}>
+        <InstaIcon width={18} height={18} />
+        <a
+          style={{ marginLeft: 10 }}
+          href="https://www.instagram.com/solbenmoll"
+          target="_blank"
+        >
+          @solbenmoll
+        </a>
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default CistellaDeProva
